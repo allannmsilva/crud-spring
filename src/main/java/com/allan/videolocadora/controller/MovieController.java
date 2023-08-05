4b5f813c65e84f725f339ebd8df4450ce3599ad7
allan.silva <allan.silva@conexos.inf.br>
2023-08-05 18:19:50 -0300

    removendo imports inúteis

--- src/main/java/com/allan/videolocadora/controller/MovieController.java
+++ src/main/java/com/allan/videolocadora/controller/MovieController.java
@@ -2,11 +2,7 @@ package com.allan.videolocadora.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
