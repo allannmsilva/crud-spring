d43d0a99ef71b09f63f8a7db489f29515e0e15cb
allan.silva <allan.silva@conexos.inf.br>
2023-08-07 11:52:36 -0300

    adicionando método de insert

--- src/main/java/com/allan/videolocadora/controller/MovieController.java
+++ src/main/java/com/allan/videolocadora/controller/MovieController.java
@@ -2,9 +2,14 @@ package com.allan.videolocadora.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.allan.videolocadora.model.Movie;
@@ -23,5 +28,12 @@ public class MovieController {
     @GetMapping
     public @ResponseBody List<Movie> getList() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public /*ResponseEntity<Movie>*/ Movie insert(@RequestBody Movie movie) {
+        return repository.save(movie);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(movie));
     }
 }
