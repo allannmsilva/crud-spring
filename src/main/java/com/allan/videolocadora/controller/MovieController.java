c082d9169beb7d4e63155394be5c69af773219c2
allan.silva <allan.silva@conexos.inf.br>
2023-08-08 21:20:07 -0300

    Removendo import não utilizado

--- src/main/java/com/allan/videolocadora/controller/MovieController.java
+++ src/main/java/com/allan/videolocadora/controller/MovieController.java
@@ -3,7 +3,6 @@ package com.allan.videolocadora.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
